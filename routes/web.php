9d2f01e9fbb27856858bfddbce72d5d5da0cf5a3
Arya Satya <aryasattya54@gmail.com>
2024-06-02 20:30:39 +0700

    route attendance

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,14 @@
 <?php
 
+use App\Http\Controllers\AttendanceController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\UsersController;
 use App\Http\Controllers\StudentsController;
-use App\Models\Students;
+
 
 Route::get('/', [DashboardController::class, 'dashboard'])->name('dashboard');
 Route::resource('users', UsersController::class);
 Route::resource('students', StudentsController::class);
+Route::resource('attendance', AttendanceController::class);
 
