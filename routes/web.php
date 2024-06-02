d7d7e7b64461f486ffdca07490282db46f2ced04
Arya Satya <aryasattya54@gmail.com>
2024-06-03 00:20:55 +0700

    mengubah format waktu pada config/app.php

--- routes/web.php
+++ routes/web.php
@@ -8,9 +8,13 @@ use App\Http\Controllers\StudentsController;
 
 
 Route::get('/', [DashboardController::class, 'dashboard'])->name('dashboard');
+
 Route::resource('users', UsersController::class);
+
 Route::resource('students', StudentsController::class);
 Route::post('students/{student}/attendance', [StudentsController::class, 'absen'])->name('students.absen');
 Route::get('students/{student}/attendance/show', [StudentsController::class, 'showAbsensi'])->name('students.showAbsensi');
+
 Route::resource('attendance', AttendanceController::class);
+Route::resource('', AttendanceController::class);
 
