7e6c247b898f923f312f7e5e27d7ce87064b888e
Arya Satya <aryasattya54@gmail.com>
2024-06-02 14:58:57 +0700

    route users

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\UsersController;
 
 
 Route::get('/', [DashboardController::class, 'dashboard'])->name('dashboard');
+Route::resource('users', UsersController::class);
+
